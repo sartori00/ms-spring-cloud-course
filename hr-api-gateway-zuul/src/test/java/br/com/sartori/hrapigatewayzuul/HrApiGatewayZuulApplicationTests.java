01d24dcae4503b0d02f5aa497d1e50f13ca6f296
Rodrigo Sartori <93sartori@gmail.com>
2022-07-29 11:24:42 -0300

    Project hr-api-gateway-zuul Created

---
+++ hr-api-gateway-zuul/src/test/java/br/com/sartori/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sartori.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
