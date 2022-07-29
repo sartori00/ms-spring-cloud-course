947056232914567c95573830810effe5d94012e4
Rodrigo Sartori <93sartori@gmail.com>
2022-07-29 06:48:47 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/br/com/sartori/hrpayroll/entities/Worker.java
@@ -0,0 +1,24 @@
+package br.com.sartori.hrpayroll.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Worker implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+
+	private Long id;
+	
+	private String  name;
+	
+	private Double dailyIncome;
+	
+	public Worker() {}
+	
+
+}
