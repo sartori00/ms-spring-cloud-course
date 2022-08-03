9d928765845fd57f2b2c2fda15b767366d5b098d
Rodrigo Sartori <93sartori@gmail.com>
2022-08-03 17:49:37 -0300

    Configs to repo

--- hr-payroll/src/main/java/br/com/sartori/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/br/com/sartori/hrpayroll/controller/PaymentController.java
@@ -1,6 +1,10 @@
 package br.com.sartori.hrpayroll.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +16,24 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import br.com.sartori.hrpayroll.model.Payment;
 import br.com.sartori.hrpayroll.services.PaymentService;
 
+@RefreshScope
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
 	
+	private static Logger logger = LoggerFactory.getLogger(PaymentController.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private PaymentService service;
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs(){
+		logger.info("Config = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
--- hr-payroll/src/main/java/br/com/sartori/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/sartori/hrpayroll/services/PaymentService.java
@@ -1,12 +1,14 @@
 package br.com.sartori.hrpayroll.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import br.com.sartori.hrpayroll.feignclients.WorkerFeignClient;
 import br.com.sartori.hrpayroll.model.Payment;
 import br.com.sartori.hrpayroll.model.Worker;
 
+@RefreshScope
 @Service
 public class PaymentService {
 	
